e7470c6cee6eff1ab36605d9dc65dc8dca2a3f15
dywane_zhu <dywanezhu@gmail.com>
2026-05-03 18:43:42 +0800

    update run_asm.sh and run_bmarks.sh

--- lab5/run_asm.sh
+++ lab5/run_asm.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+allowed_args=("ONECYCLE" "TWOCYCLE" "FOURCYCLE" "TWOSTAGE" "TWOSTAGEBTB")
+
+log_suffix="$1"
+
+# 检查是否输入参数
+if [ -z "$log_suffix" ]; then
+    echo "错误：请输入一个参数！"
+    echo "允许的值：${allowed_args[*]}"
+    exit 1
+fi
+
+# 检查参数是否合法
+valid=0
+for a in "${allowed_args[@]}"; do
+    if [ "$a" = "$log_suffix" ]; then
+        valid=1
+        break
+    fi
+done
+
+if [ $valid -eq 0 ]; then
+    echo "错误：输入参数不合法！"
+    echo "允许的值：${allowed_args[*]}"
+    exit 1
+fi
 
 asm_tests=(
 	simple
@@ -23,8 +48,9 @@ asm_tests=(
 	)
 
 vmh_dir=programs/build/assembly/vmh
-log_dir=logs-asm
+log_dir="logs-${log_suffix}"
 wait_time=3
+count=0
 
 # create bsim log dir
 mkdir -p ${log_dir}
--- lab5/run_bmarks.sh
+++ lab5/run_bmarks.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+allowed_args=("ONECYCLE" "TWOCYCLE" "FOURCYCLE" "TWOSTAGE" "TWOSTAGEBTB")
+
+log_suffix="$1"
+
+# 检查是否输入参数
+if [ -z "$log_suffix" ]; then
+    echo "错误：请输入一个参数！"
+    echo "允许的值：${allowed_args[*]}"
+    exit 1
+fi
+
+# 检查参数是否合法
+valid=0
+for a in "${allowed_args[@]}"; do
+    if [ "$a" = "$log_suffix" ]; then
+        valid=1
+        break
+    fi
+done
+
+if [ $valid -eq 0 ]; then
+    echo "错误：输入参数不合法！"
+    echo "允许的值：${allowed_args[*]}"
+    exit 1
+fi
+
 asm_tests=(
 	median
 	multiply
@@ -9,8 +35,9 @@ asm_tests=(
 	)
 
 vmh_dir=programs/build/benchmarks/vmh
-log_dir=logs-bmarks
+log_dir="logs-${log_suffix}"
 wait_time=3
+count=35
 
 # create bsim log dir
 mkdir -p ${log_dir}
